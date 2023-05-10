a5c7eea211c186035c579277309f344a85691ea2
FeiPetr <777spetrova@gmail.com>
2023-05-09 18:02:28 -0700

    character jumps

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,10 +6,10 @@ class Play extends Phaser.Scene{ //creating js class 'menu' that extends phaser'
 
     preload() {
         // load images/tile sprites
-        this.load.image('character', './assets/rocket.png');
+        this.load.image('character', './assets/cat.PNG');
 
         // add more tile sprites here and replace, etc etc
-        this.load.image('platform','./assets/tilesprite.jpg');
+        this.load.image('platform','./assets/board.PNG');
         // load spritesheet
         //i should probably learn how spritesheets actually work
         //this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
@@ -19,51 +19,61 @@ class Play extends Phaser.Scene{ //creating js class 'menu' that extends phaser'
         // place tile sprite
         this.bgtile = this.add.tileSprite(0, 0, 640, 480, 'bgtile').setOrigin(0, 0);
 
-// white borders
-        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         // add runner
         //this.character = new Character(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'character').setOrigin(0.5, 0);
-        this.character = this.physics.add.sprite(this.sys.game.config.width / 2, 0, 'character');
+        this.character = this.physics.add.sprite(this.sys.game.config.width / 2, this.sys.game.config.height/2, 'character');
         
+        this.ground = this.physics.add.staticSprite(this.sys.game.config.width / 2, this.sys.game.config.height * .95, 'platform');
 
-        let ground = this.physics.add.sprite(this.sys.game.config.width / 2, this.sys.game.config.height * .95, "platform");
-        ground.displayWidth = this.sys.game.config.width * 1.1;
-        ground.setImmovable();
+        this.ground.displayWidth = this.sys.game.config.width * 1.1;
+        //this.ground.setImmovable();
         //
         //
         //
+        this.power = 0;
         //set collider
         //
         //
-        this.physics.add.collider(this.ball, ground);
+        this.physics.add.collider(this.character, this.ground);
 
         // add tiles
         //this.platform = new Platform(this, game.config.width + borderUISize*6, borderUISize*4, 'platform', 0, 30).setOrigin(0, 0);
 
         // define keys
-        keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-
+        keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
         // GAME OVER flag
         this.gameOver = false; 
         
 
      }
+
+///// start jump functions /////
+
+  startJump() {
+    //console.log("need to beat " + this.sys.game.config.height * .95);
+    //console.log(this.character.y);
+
+     this.character.setVelocityY(-200);
+
+  }
+
+
      update() {
         // check key input r for restart
+        
         if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
           this.scene.restart();
         }
 
-        //make background scroll
-        //this.starfield.tilePositionX -= 4;
-
+        this.bgtile.tilePositionX += 2; // making background scroll
 
         if(!this.gameOver){
           // update tiles appearing?
+          if (Phaser.Input.Keyboard.JustDown(keyUP)) {
+            this.startJump();
+          }
+  
         }
       }
   
